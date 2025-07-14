f3f6cc911e2d7bdedfdf9189ef9eed271da82bd3
sofia.kilipko <sofia.kilipko@gmail.com>
2025-07-14 16:23:03 +0100

    adding api integration, imported axios

--- src/script.js
+++ src/script.js
@@ -1,16 +1,30 @@
 // import Typewriter from 'typewriter-effect';
 
+const displayResponse = (response) => {
+    console.log(response);
+    new Typewriter("#response", {
+        autoStart: true,
+        delay: 5,
+        cursor: ""
+    }).typeString(response.data.answer + '<br> SheCodes AI')
+        // ⛔ do NOT call .pauseFor() or .deleteAll()
+        .start();;
+}
 const generatePoem = (event) => {
     event.preventDefault();
 
-    let text = "hello";
+    document.getElementById("response").innerHTML = "Generating, please wait...";
 
-    new Typewriter("#response", {
-        strings : [text],
-        autoStart : true,
-        delay : 5,
-        cursor : ""
-    });
+    let instructions = document.querySelector("#user_instructions").value;
+
+    let key = "dc4fa97dd0aa64407o93bea76f4taaab";
+    let context = "answer should be maximum 3 paragraphs long, well structured and be readable and fully followed to what was asked in prompt. if no topic was given just ask to provide them";
+    let prompt = `generate a short poem on a ${instructions}  topic`;
+    let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+    console.log("generating");
+
+    axios.get(url).then(displayResponse);
 }
 
 let poemForm = document.querySelector("#inputForm");
